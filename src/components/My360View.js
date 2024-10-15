0e342d6fe172e73858bec807af35e0195785b9c7
PhamQuang123 <phamtienquang57@gmail.com>
2024-10-15 23:13:45 +0700

    successful

--- src/components/My360View.js
+++ src/components/My360View.js
@@ -3,7 +3,6 @@ import React360Viewer from "react-360-view";
 
 const My360View = () => {
   const [index, setIndex] = useState(1);
-  const [fileName, setFileName] = useState("DJI_0001.JPG");
   const totalFrames = 26;
   const isDragging = useRef(false); // Theo dõi trạng thái giữ chuột
   const startX = useRef(0); // Lưu vị trí chuột khi bắt đầu kéo
@@ -46,19 +45,13 @@ const My360View = () => {
   const handleMouseUp = () => {
     isDragging.current = false; // Dừng việc theo dõi chuột
   };
-  //   const getFileName = (index) => {
-  //     // return `DJI_00${index < 10 ? `0${index}` : index}.JPG`;
-  //     return `DJI_000${index}.JPG`;
-  //   };
 
-  //   useEffect(() => {
-  //     const newFileName = `DJI_00${index < 10 ? `0${index}` : index}.JPG`;
-  //     setFileName(newFileName);
-  //     console.log(fileName);
-  //   }, [index]); // Cập nhật fileName khi index thay đổi
+  const fileNamexxx = (index < 10 ? "DJI_000" : "DJI_00") + index + ".JPG";
+
   return (
     <div>
-      <h1>360 Image Viewer</h1>
+      <h1>360 Image_01 Viewer </h1>
+
       <div
         // onMouseDown={handleMouseDown} // Lắng nghe sự kiện click giữ chuột
         // onMouseMove={handleMouseMove} // Lắng nghe sự kiện di chuyển chuột
@@ -66,42 +59,32 @@ const My360View = () => {
         // onMouseLeave={handleMouseUp} // Dừng xoay nếu chuột rời khỏi vùng ảnh
         style={{ width: "700px", height: "650px" }} // Đặt kích thước viewer
       >
-        {index < 10 ? (
-          <React360Viewer
-            amount={totalFrames}
-            imagePath="/100_0404"
-            fileName="DJI_000{index}.JPG"
-            index={index} // Giá trị index tự động thay đổi
-            autoplay={false} // Tắt tự động xoay
-            notifyOnPointerDown={handleMouseDown}
-            notifyOnPointerMoved={handleMouseMove}
-            notifyOnPointerUp={handleMouseUp}
-          />
-        ) : (
-          <React360Viewer
-            amount={totalFrames}
-            imagePath="/100_0404"
-            fileName="DJI_00{index}.JPG"
-            index={index} // Giá trị index tự động thay đổi
-            autoplay={false} // Tắt tự động xoay
-            notifyOnPointerDown={handleMouseDown}
-            notifyOnPointerMoved={handleMouseMove}
-            notifyOnPointerUp={handleMouseUp}
-          />
-        )}
-      </div>
-      {/* <div>
         <React360Viewer
-          imagesBaseUrl="/100_0404  "
-          imagesCount={totalFrames} // Số lượng hình ảnh
-          imagesFiletype="jpg" // Định dạng file
-          imageNames={imageNames}
-          mouseDragSpeed={10}
+          amount={totalFrames}
+          imagePath="/100_0404"
+          paddingIndex={true} // chuyển đổi index thành số có 1 chữ số thành 2 chữ số 1-9 -> 01-09
+          fileName="DJI_00{index}.JPG"
+          index={index} // Giá trị index tự động thay đổi
+          autoplay={false} // Tắt tự động xoay
           notifyOnPointerDown={handleMouseDown}
           notifyOnPointerMoved={handleMouseMove}
           notifyOnPointerUp={handleMouseUp}
         />
-      </div> */}
+      </div>
+      <h1>360 Image_02 Viewer</h1>
+      <div style={{ width: "800px", height: "1000px" }}>
+        <React360Viewer
+          amount={totalFrames}
+          imagePath="/100_0405"
+          fileName="DJI_00{index}.JPG"
+          index={index}
+          paddingIndex={true}
+          autoplay={false}
+          notifyOnPointerDown={handleMouseDown}
+          notifyOnPointerMoved={handleMouseMove}
+          notifyOnPointerUp={handleMouseUp}
+        />
+      </div>
     </div>
   );
 };
