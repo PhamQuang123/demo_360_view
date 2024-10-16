2cb307de237280fe6034e2b2cd8e2dceec6f5288
PhamQuang123 <phamtienquang57@gmail.com>
2024-10-16 12:32:38 +0700

    my360view

--- src/components/My360View.js
+++ src/components/My360View.js
@@ -4,21 +4,21 @@ import React360Viewer from "react-360-view";
 const My360View = () => {
   const [index, setIndex] = useState(1);
   const totalFrames = 26;
-  const isDragging = useRef(false); // Theo dõi trạng thái giữ chuột
-  const startX = useRef(0); // Lưu vị trí chuột khi bắt đầu kéo
-  const startIndex = useRef(0); // Lưu giá trị index ban đầu khi kéo bắt đầu
+  const isDragging = useRef(false);
+  const startX = useRef(0);
+  const startIndex = useRef(0);
 
   // Hàm xử lý sự kiện khi người dùng click giữ chuột
   const handleMouseDown = (e) => {
     isDragging.current = true;
-    startX.current = e.clientX; // Lưu vị trí X ban đầu của chuột
-    startIndex.current = index; // Lưu index ban đầu khi bắt đầu kéo
+    startX.current = e.clientX;
+    startIndex.current = index;
   };
 
   // Hàm xử lý sự kiện khi chuột di chuyển trong khi giữ click
   const handleMouseMove = (e) => {
     if (isDragging.current) {
-      const dx = e.clientX - startX.current; // Tính khoảng cách kéo
+      const dx = e.clientX - startX.current;
       const sensitivity = 5; // Độ nhạy khi kéo chuột (tăng số này để xoay nhanh hơn)
       let newIndex = startIndex.current + Math.floor(dx / sensitivity);
 
@@ -30,10 +30,9 @@ const My360View = () => {
       }
 
       if (dx < 0) {
-        // Di chuyển chuột sang trái -> giảm frame
         setIndex((prevFrame) => (prevFrame > 1 ? prevFrame - 1 : totalFrames));
       } else if (dx > 0) {
-        // Di chuyển chuột sang phải -> tăng frame
+        // Di chuyển chuột sang phải -> tăng fram
         setIndex((prevFrame) => (prevFrame < totalFrames ? prevFrame + 1 : 1));
       }
     }
@@ -49,18 +48,18 @@ const My360View = () => {
       <h1>360 Image_01 Viewer </h1>
 
       <div
-        // onMouseDown={handleMouseDown} // Lắng nghe sự kiện click giữ chuột
-        // onMouseMove={handleMouseMove} // Lắng nghe sự kiện di chuyển chuột
-        // onMouseUp={handleMouseUp} // Dừng xoay khi nhả chuột
-        // onMouseLeave={handleMouseUp} // Dừng xoay nếu chuột rời khỏi vùng ảnh
-        style={{ width: "700px", height: "650px" }} // Đặt kích thước viewer
+        // onMouseDown={handleMouseDown}
+        // onMouseMove={handleMouseMove}
+        // onMouseUp={handleMouseUp}
+        // onMouseLeave={handleMouseUp}
+        style={{ width: "700px", height: "650px" }}
       >
         <React360Viewer
           amount={totalFrames}
           imagePath="/100_0404"
           paddingIndex={true} // chuyển đổi index thành số có 1 chữ số thành 2 chữ số 1-9 -> 01-09
           fileName="DJI_00{index}.JPG"
-          index={index} // Giá trị index tự động thay đổi
+          index={index}
           autoplay={false} // Tắt tự động xoay
           notifyOnPointerDown={handleMouseDown}
           notifyOnPointerMoved={handleMouseMove}
